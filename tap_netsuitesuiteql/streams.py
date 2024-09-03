a02cab15ac17d819b47d6909be788a16c4ef51c7
quentinlumapps <quentin.derunes@lumapps.com>
2024-09-03 15:44:35 +0200

    ARR history: report region + reseller + partner
    BI-1

--- tap_netsuitesuiteql/streams.py
+++ tap_netsuitesuiteql/streams.py
@@ -21,7 +21,7 @@ class ArrHistoryStream(NetsuiteSuiteQLStream):
     name = "arr_history"
     path = ""
     primary_keys = ["unique_key"]
-    query = "SELECT TL.uniqueKey unique_key, T.id id, T.trandate arr_date, TL.netamount, T.currency arr_currency, AT.name arr_type, CE.id enduser_id, CE.custentity_lum_cus_sfid enduser_sfid, LT.tranid so_tranid, LT.currency so_currency, LT.exchangerate so_exchange_rate, CR.custentity_lum_cus_sfid reseller_sfid, CP.custentity_lum_cus_sfid partner_sfid, S.name subsidiary_name, S.currency subsidiary_currency, CT.name contract_name, CT.id contract_id, CEP.custentity_lum_cus_sfid enduser_parent_sfid, CEGP.custentity_lum_cus_sfid enduser_grandparent_sfid, S.custrecord_lum_fixedfxusd_sub subsidiary_exchange_rate, FROM transactionline TL  LEFT JOIN transaction T ON TL.transaction = T.id  LEFT JOIN customlist_lum_arrh_arrtype AT ON AT.id = T.custbody_lum_arrh_arrtype  LEFT JOIN customer CE ON CE.id = T.custbody_lum_arrh_enduser LEFT JOIN customrecord_prq_contract CT ON CT.id = T.custbody_lum_arrh_contract LEFT JOIN customer CR ON CR.id = T.custbody_lum_arrh_reseller LEFT JOIN customer CP ON CP.id = T.custbody_lum_arrh_partner LEFT JOIN subsidiary S ON S.id = TL.subsidiary LEFT JOIN transaction LT ON LT.id = T.custbody_lum_arrh_transaction LEFT JOIN customer CEP ON CEP.id = CE.parent LEFT JOIN customer CEGP ON CEGP.id = CEP.parent WHERE AT.name IN ('ARR', 'cARR') AND T.recordtype = 'customtransaction_lum_arrh'  AND TL.mainline = 'F'"
+    query = "SELECT TL.uniqueKey unique_key, T.id id, T.trandate arr_date, TL.netamount, T.currency arr_currency, AT.name arr_type, CE.id enduser_id, CE.custentity_lum_cus_sfid enduser_sfid, LT.tranid so_tranid, LT.currency so_currency, LT.exchangerate so_exchange_rate, CR.custentity_lum_cus_sfid reseller_sfid, CR.companyname reseller_name, CP.custentity_lum_cus_sfid partner_sfid, CP.companyname partner_name, S.name subsidiary_name, S.currency subsidiary_currency, CT.name contract_name, CT.id contract_id, CEP.custentity_lum_cus_sfid enduser_parent_sfid, CEGP.custentity_lum_cus_sfid enduser_grandparent_sfid, S.custrecord_lum_fixedfxusd_sub subsidiary_exchange_rate, RR.name report_region FROM transactionline TL  LEFT JOIN transaction T ON TL.transaction = T.id  LEFT JOIN customlist_lum_arrh_arrtype AT ON AT.id = T.custbody_lum_arrh_arrtype  LEFT JOIN customer CE ON CE.id = T.custbody_lum_arrh_enduser LEFT JOIN customrecord_prq_contract CT ON CT.id = T.custbody_lum_arrh_contract LEFT JOIN customer CR ON CR.id = T.custbody_lum_arrh_reseller LEFT JOIN customer CP ON CP.id = T.custbody_lum_arrh_partner LEFT JOIN subsidiary S ON S.id = TL.subsidiary LEFT JOIN transaction LT ON LT.id = T.custbody_lum_arrh_transaction LEFT JOIN customer CEP ON CEP.id = CE.parent LEFT JOIN customer CEGP ON CEGP.id = CEP.parent LEFT JOIN CUSTOMLIST_LUM_ARRH_REGION RR ON RR.id=T. custbody_lum_arrh_region WHERE AT.name IN ('ARR', 'cARR') AND T.recordtype = 'customtransaction_lum_arrh'  AND TL.mainline = 'F'"
     replication_key = None
 
     schema = th.PropertiesList(
@@ -37,7 +37,9 @@ class ArrHistoryStream(NetsuiteSuiteQLStream):
         th.Property("so_currency", th.IntegerType),
         th.Property("so_exchange_rate", th.NumberType),
         th.Property("reseller_sfid", th.StringType),
+        th.Property("reseller_name", th.StringType),
         th.Property("partner_sfid", th.StringType),
+        th.Property("partner_name", th.StringType),
         th.Property("subsidiary_name", th.StringType),
         th.Property("subsidiary_currency", th.IntegerType),
         th.Property("contract_name", th.StringType),
@@ -45,6 +47,7 @@ class ArrHistoryStream(NetsuiteSuiteQLStream):
         th.Property("enduser_parent_sfid", th.StringType),
         th.Property("enduser_grandparent_sfid", th.StringType),
         th.Property("subsidiary_exchange_rate", th.NumberType),
+        th.Property("report_region", th.StringType),
 
     ).to_dict()
 
