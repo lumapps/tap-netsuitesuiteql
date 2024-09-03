bb95f82ce6c84d069dd3ee7f63cf64fc60e163d9
quentinlumapps <quentin.derunes@lumapps.com>
2024-09-03 08:23:34 +0200

    geographical data stream + fixed snake case field
    BI-1

--- tap_netsuitesuiteql/streams.py
+++ tap_netsuitesuiteql/streams.py
@@ -20,35 +20,19 @@ class ArrHistoryStream(NetsuiteSuiteQLStream):
 
     name = "arr_history"
     path = ""
-    primary_keys = ["uniqueKey"]
-    query = "SELECT TL.uniqueKey, T.id id, T.trandate arr_date, TL.netamount, T.currency arr_currency, AT.name arr_type, CE.id enduser_id, CE.custentity_lum_cus_sfid enduser_sfid, LT.tranid so_tranid, LT.currency so_currency, LT.exchangerate so_exchange_rate, CR.custentity_lum_cus_sfid reseller_sfid, CP.custentity_lum_cus_sfid partner_sfid, S.name subsidiary_name, S.currency subsidiary_currency, CT.name contract_name, CT.id contract_id, CEP.custentity_lum_cus_sfid enduser_parent_sfid, CEGP.custentity_lum_cus_sfid enduser_grandparent_sfid, S.custrecord_lum_fixedfxusd_sub subsidiary_exchange_rate, FROM transactionline TL  LEFT JOIN transaction T ON TL.transaction = T.id  LEFT JOIN customlist_lum_arrh_arrtype AT ON AT.id = T.custbody_lum_arrh_arrtype  LEFT JOIN customer CE ON CE.id = T.custbody_lum_arrh_enduser LEFT JOIN customrecord_prq_contract CT ON CT.id = T.custbody_lum_arrh_contract LEFT JOIN customer CR ON CR.id = T.custbody_lum_arrh_reseller LEFT JOIN customer CP ON CP.id = T.custbody_lum_arrh_partner LEFT JOIN subsidiary S ON S.id = TL.subsidiary LEFT JOIN transaction LT ON LT.id = T.custbody_lum_arrh_transaction LEFT JOIN customer CEP ON CEP.id = CE.parent LEFT JOIN customer CEGP ON CEGP.id = CEP.parent WHERE AT.name IN ('ARR', 'cARR') AND T.recordtype = 'customtransaction_lum_arrh'  AND TL.mainline = 'F'"
+    primary_keys = ["unique_key"]
+    query = "SELECT TL.uniqueKey unique_key, T.id id, T.trandate arr_date, TL.netamount, T.currency arr_currency, AT.name arr_type, CE.id enduser_id, CE.custentity_lum_cus_sfid enduser_sfid, LT.tranid so_tranid, LT.currency so_currency, LT.exchangerate so_exchange_rate, CR.custentity_lum_cus_sfid reseller_sfid, CP.custentity_lum_cus_sfid partner_sfid, S.name subsidiary_name, S.currency subsidiary_currency, CT.name contract_name, CT.id contract_id, CEP.custentity_lum_cus_sfid enduser_parent_sfid, CEGP.custentity_lum_cus_sfid enduser_grandparent_sfid, S.custrecord_lum_fixedfxusd_sub subsidiary_exchange_rate, FROM transactionline TL  LEFT JOIN transaction T ON TL.transaction = T.id  LEFT JOIN customlist_lum_arrh_arrtype AT ON AT.id = T.custbody_lum_arrh_arrtype  LEFT JOIN customer CE ON CE.id = T.custbody_lum_arrh_enduser LEFT JOIN customrecord_prq_contract CT ON CT.id = T.custbody_lum_arrh_contract LEFT JOIN customer CR ON CR.id = T.custbody_lum_arrh_reseller LEFT JOIN customer CP ON CP.id = T.custbody_lum_arrh_partner LEFT JOIN subsidiary S ON S.id = TL.subsidiary LEFT JOIN transaction LT ON LT.id = T.custbody_lum_arrh_transaction LEFT JOIN customer CEP ON CEP.id = CE.parent LEFT JOIN customer CEGP ON CEGP.id = CEP.parent WHERE AT.name IN ('ARR', 'cARR') AND T.recordtype = 'customtransaction_lum_arrh'  AND TL.mainline = 'F'"
     replication_key = None
 
     schema = th.PropertiesList(
-        th.Property("uniqueKey", th.IntegerType),
-
-        th.Property(
-            "id",
-            th.IntegerType,
-        ),
-        th.Property(
-            "arr_date",
-            th.DateType,
-        ),
-        th.Property(
-            "netamount",
-            th.NumberType,
-        ),
+        th.Property("unique_key", th.IntegerType),
+        th.Property("id", th.IntegerType),
+        th.Property("arr_date", th.DateType),
+        th.Property("netamount", th.NumberType),
         th.Property("arr_currency", th.IntegerType),
         th.Property("arr_type", th.StringType),
-        th.Property(
-            "enduser_id",
-            th.IntegerType,
-        ),
-        th.Property(
-            "enduser_sfid",
-            th.StringType,
-        ),
+        th.Property("enduser_id", th.IntegerType),
+        th.Property("enduser_sfid", th.StringType),
         th.Property("so_tranid", th.StringType),
         th.Property("so_currency", th.IntegerType),
         th.Property("so_exchange_rate", th.NumberType),
@@ -71,16 +55,37 @@ class EndusersStream(NetsuiteSuiteQLStream):
     name = "endusers"
     path = ""
     primary_keys = ["id"]
-    query = "SELECT C.id, C.custentity_lum_cus_sfid enduser_sfid, C.companyName as companyname, C.firstOrderDate as first_order_date, S.id as subsidiary_id, S.name as subsidiary_name FROM customer C LEFT JOIN customerSubsidiaryRelationship SR ON SR.entity=C.id LEFT JOIN Subsidiary S ON S.id=SR.subsidiary WHERE C.custentity_prq_end_user='T'"
+    query = "SELECT C.id, C.custentity_lum_cus_sfid enduser_sfid, C.companyName as companyname, CY.id as country_code, CY.name as country, state.shortname state_shortname, state.fullname state_fullname, C.firstOrderDate as first_order_date, S.id as subsidiary_id, S.name as subsidiary_name  FROM customer C  LEFT JOIN customerSubsidiaryRelationship SR ON SR.entity=C.id  LEFT JOIN Subsidiary S ON S.id=SR.subsidiary  LEFT JOIN EntityAddress ADD ON C. defaultBillingAddress=ADD. nKey LEFT JOIN Country CY ON ADD.country=CY.id LEFT JOIN state ON ADD.country = state.country AND ADD.dropdownstate = state.shortname WHERE C.custentity_prq_end_user='T'"
     replication_key = None
 
     schema = th.PropertiesList(
         th.Property("id", th.IntegerType),
         th.Property("enduser_sfid", th.StringType),
         th.Property("companyname", th.StringType),
+        th.Property("country_code", th.StringType),
+        th.Property("country", th.StringType),
+        th.Property("state_shortname", th.StringType),
+        th.Property("state_fullname", th.StringType),
         th.Property("subsidiary_id", th.IntegerType),
         th.Property("subsidiary_name", th.StringType),
         th.Property("first_order_date", th.DateType),
+
+    ).to_dict()
+
+class GeographicalHierarchyStream(NetsuiteSuiteQLStream):
+    """Define custom stream."""
+
+    name = "geography"
+    path = ""
+    primary_keys = ["id"]
+    query = "SELECT R.recordid as id, R.name, RT.name as type, R.parent as parent FROM CUSTOMRECORD_LUM_REGION_CORPORATE R LEFT JOIN CUSTOMLIST_LUM_GEOGRAPHY_TYPE RT ON RT.id = R.custrecord_lum_region_corporate_type WHERE R.isinactive='F'"
+    replication_key = None
+
+    schema = th.PropertiesList(
+        th.Property("id", th.IntegerType),
+        th.Property("name", th.StringType),
+        th.Property("type", th.StringType),
+        th.Property("parent", th.IntegerType),
 
     ).to_dict()
 
@@ -90,12 +95,12 @@ class TransactionLineStream(NetsuiteSuiteQLStream):
 
     name = "transaction_line"
     path = ""
-    primary_keys = ["uniqueKey"]
-    query = "SELECT TL.uniqueKey, T.id, BUILTIN.DF(T.type) type, BUILTIN.DF(T.status) status, BUILTIN.DF(T.custbody_prq_approval_custom_status) approvalStatus, T.tranId, T.tranDate, T.custbody_prq_end_user endUserId, BUILTIN.DF(T.custbody_prq_end_user) endUser, T.entity billToCustomerId, BUILTIN.DF(T.entity) billToCustomer, T.custbody_prq_contract contractId, BUILTIN.DF(T.custbody_prq_contract) contract, BUILTIN.DF(C.custrecord_prq_ct_partner) partner, BUILTIN.DF(T.custbody_prq_contract_type) contractType, BUILTIN.DF(C.custrecord_prq_ct_geographical_area) contractArea, BUILTIN.DF(T.currency) currency, T.exchangeRate, BUILTIN.DF(TL.subsidiary) subsidiary, S.custrecord_lum_fixedfxusd_sub fixedExchangeRateUsd, T.startDate tranStartDate, T.endDate tranEndDate, T.custbody_prq_months tranMonths, BUILTIN.DF(T.cseg_prq_revenue) revenue, BUILTIN.DF(TL.location) location, T.custbody_prq_carr carr, T.custbody_lum_incrementalcarr incrementalCarr, T.custbody_prq_carr_start_date carrStartDate, T.custbody_prq_carr_end_date carrEndDate, BUILTIN.DF(T.custbody_prq_product) mainProduct, BUILTIN.DF(T.custbody_prq_customer_success_manager) csm, BUILTIN.DF(T.employee) salesRep, T.custbody_prq_renewal renewal, T.custbody_prq_renewal_start_date renewalStartDate, T.custbody_prq_renewal_end_date renewalEndDate, T.custbody_prq_renewal_months renewalMonths, T.custbody_prq_renewed renewed, BUILTIN.DF(T.custbody_prq_renewal_linked_trans) renewalEstimate, TL.id lineId, TL.custcol_prq_billing_so_line line, BUILTIN.DF(TL.cseg_prq_revenue) lineRevenue, TL.custcol_lum_hubspot_line hubspotLine, TL.item itemId, BUILTIN.DF(TL.item) item, BUILTIN.DF(I.custitem_lum_productfamily_ma) productFamily, TL.custcol_prq_users users, TL.quantity amountExcludingTax, TL.rate quantity, TL.netAmount, TL.memo description, TL.isclosed, TL.custcol_prq_start_date lineStartDate, TL.custcol_prq_end_date lineEndDate, TL.custcol_prq_months lineMonths, TL.custcol_prq_renewal lineRenewal, TL.custcol_prq_arr_calculation arrCalc, BUILTIN.DF(TL.custcolcustcol_typeheyaxel) typeHeyAxel, T.createdDate, TL.custcol_prq_billing_period_from billingPeriodFrom, TL.custcol_prq_billing_period_to billingPeriodTo FROM transaction T LEFT JOIN transactionline TL ON TL.transaction = T.id LEFT JOIN customer CE ON CE.id = T.custbody_prq_end_user LEFT JOIN item I ON I.id = TL.item LEFT JOIN subsidiary S ON S.id = TL.subsidiary LEFT JOIN customrecord_prq_contract C ON C.id = T.custbody_prq_contract WHERE BUILTIN.DF(T.type) IN ('Estimate','Sales Order') AND TL.mainLine = 'F' AND TL.taxLine = 'F'"
+    primary_keys = ["unique_key"]
+    query = "SELECT TL.uniqueKey unique_key, T.id, BUILTIN.DF(T.type) type, BUILTIN.DF(T.status) status, BUILTIN.DF(T.custbody_prq_approval_custom_status) approvalStatus, T.tranId, T.tranDate, T.custbody_prq_end_user endUserId, BUILTIN.DF(T.custbody_prq_end_user) endUser, T.entity billToCustomerId, BUILTIN.DF(T.entity) billToCustomer, T.custbody_prq_contract contractId, BUILTIN.DF(T.custbody_prq_contract) contract, BUILTIN.DF(C.custrecord_prq_ct_partner) partner, BUILTIN.DF(T.custbody_prq_contract_type) contractType, BUILTIN.DF(C.custrecord_prq_ct_geographical_area) contractArea, BUILTIN.DF(T.currency) currency, T.exchangeRate, BUILTIN.DF(TL.subsidiary) subsidiary, S.custrecord_lum_fixedfxusd_sub fixedExchangeRateUsd, T.startDate tranStartDate, T.endDate tranEndDate, T.custbody_prq_months tranMonths, BUILTIN.DF(T.cseg_prq_revenue) revenue, BUILTIN.DF(TL.location) location, T.custbody_prq_carr carr, T.custbody_lum_incrementalcarr incrementalCarr, T.custbody_prq_carr_start_date carrStartDate, T.custbody_prq_carr_end_date carrEndDate, BUILTIN.DF(T.custbody_prq_product) mainProduct, BUILTIN.DF(T.custbody_prq_customer_success_manager) csm, BUILTIN.DF(T.employee) salesRep, T.custbody_prq_renewal renewal, T.custbody_prq_renewal_start_date renewalStartDate, T.custbody_prq_renewal_end_date renewalEndDate, T.custbody_prq_renewal_months renewalMonths, T.custbody_prq_renewed renewed, BUILTIN.DF(T.custbody_prq_renewal_linked_trans) renewalEstimate, TL.id lineId, TL.custcol_prq_billing_so_line line, BUILTIN.DF(TL.cseg_prq_revenue) lineRevenue, TL.custcol_lum_hubspot_line hubspotLine, TL.item itemId, BUILTIN.DF(TL.item) item, BUILTIN.DF(I.custitem_lum_productfamily_ma) productFamily, TL.custcol_prq_users users, TL.quantity amountExcludingTax, TL.rate quantity, TL.netAmount, TL.memo description, TL.isclosed, TL.custcol_prq_start_date lineStartDate, TL.custcol_prq_end_date lineEndDate, TL.custcol_prq_months lineMonths, TL.custcol_prq_renewal lineRenewal, TL.custcol_prq_arr_calculation arrCalc, BUILTIN.DF(TL.custcolcustcol_typeheyaxel) typeHeyAxel, T.createdDate, TL.custcol_prq_billing_period_from billingPeriodFrom, TL.custcol_prq_billing_period_to billingPeriodTo FROM transaction T LEFT JOIN transactionline TL ON TL.transaction = T.id LEFT JOIN customer CE ON CE.id = T.custbody_prq_end_user LEFT JOIN item I ON I.id = TL.item LEFT JOIN subsidiary S ON S.id = TL.subsidiary LEFT JOIN customrecord_prq_contract C ON C.id = T.custbody_prq_contract WHERE BUILTIN.DF(T.type) IN ('Estimate','Sales Order') AND TL.mainLine = 'F' AND TL.taxLine = 'F'"
     replication_key = None
 
     schema = th.PropertiesList(
-        th.Property("uniqueKey", th.IntegerType),
+        th.Property("unique_key", th.IntegerType),
         th.Property("id", th.IntegerType),
         th.Property("type", th.StringType),
         th.Property("status", th.StringType),
