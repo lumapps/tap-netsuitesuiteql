0feb4f362dd1dc48f77651138cf38177174f821a
quentinlumapps <quentin.derunes@lumapps.com>
2025-01-09 09:11:00 +0100

    added partner, reseller to endusers
    BI-175

--- tap_netsuitesuiteql/streams.py
+++ tap_netsuitesuiteql/streams.py
@@ -59,7 +59,7 @@ class EndusersStream(NetsuiteSuiteQLStream):
     name = "endusers"
     path = ""
     primary_keys = ["id"]
-    query = "SELECT C.id id, C.custentity_lum_cus_sfid enduser_sfid, C.companyName as companyname, CY.id as country_code, CY.name as country, state.shortname state_shortname, state.fullname state_fullname, C.firstOrderDate as first_order_date, S.id as subsidiary_id, S.name as subsidiary_name, CEP.id as enduser_parent_id, CEP.companyname as enduser_parent_name, CEP.firstOrderDate as parent_first_order_date  FROM customer C LEFT JOIN customer CEP ON CEP.id = C.parent LEFT JOIN customerSubsidiaryRelationship SR ON SR.entity=C.id LEFT JOIN Subsidiary S ON S.id=SR.subsidiary  LEFT JOIN EntityAddress ADD ON C. defaultBillingAddress=ADD. nKey LEFT JOIN Country CY ON ADD.country=CY.id LEFT JOIN state ON ADD.country = state.country AND ADD.dropdownstate = state.shortname WHERE (C.custentity_prq_end_user='T' OR C.custentity_prq_partner='T' OR C.custentity_prq_reseller='T') ORDER BY id"
+    query = "SELECT C.id id, C.custentity_lum_cus_sfid enduser_sfid, C.companyName as companyname, CY.id as country_code, CY.name as country, state.shortname state_shortname, state.fullname state_fullname, C.firstOrderDate as first_order_date, S.id as subsidiary_id, S.name as subsidiary_name, CEP.id as enduser_parent_id, CEP.companyname as enduser_parent_name, CEP.firstOrderDate as parent_first_order_date, C.custentity_prq_end_user is_enduser, C.custentity_prq_partner is_partner, C.custentity_prq_reseller is_reseller FROM customer C LEFT JOIN customer CEP ON CEP.id = C.parent LEFT JOIN customerSubsidiaryRelationship SR ON SR.entity=C.id LEFT JOIN Subsidiary S ON S.id=SR.subsidiary  LEFT JOIN EntityAddress ADD ON C. defaultBillingAddress=ADD. nKey LEFT JOIN Country CY ON ADD.country=CY.id LEFT JOIN state ON ADD.country = state.country AND ADD.dropdownstate = state.shortname WHERE (C.custentity_prq_end_user='T' OR C.custentity_prq_partner='T' OR C.custentity_prq_reseller='T') ORDER BY id"
     replication_key = None
 
     schema = th.PropertiesList(
@@ -76,6 +76,9 @@ class EndusersStream(NetsuiteSuiteQLStream):
         th.Property("subsidiary_name", th.StringType),
         th.Property("first_order_date", th.DateType),
         th.Property("parent_first_order_date", th.DateType),
+        th.Property("is_enduser", th.StringType),
+        th.Property("is_partner", th.StringType),
+        th.Property("is_reseller", th.StringType),
 
     ).to_dict()
 
